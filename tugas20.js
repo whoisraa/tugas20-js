8a0a2f9531dae887b8d5e4a8373df3caa2113d9f
whoisraa <raainvictus@gmail.com>
2021-11-11 10:41:33 +0700

    tugas 20 js

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+const pencarianKata = (satu, dua) => {
+    let kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+    console.log(kalimat.match(satu));
+    console.log(kalimat.match(dua));
+}
+
+pencarianKata(/are/, /words/);
